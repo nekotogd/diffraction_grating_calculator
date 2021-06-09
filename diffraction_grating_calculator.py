171b2d60c00176edcf09d88175a34bfae686d701
nekotogd <81257780+nekotogd@users.noreply.github.com>
2021-06-09 11:57:32 +0400

    Add files via upload

--- diffraction_grating_calculator.py
+++ diffraction_grating_calculator.py
@@ -7,7 +7,7 @@ import math #Math library so we can do trigs and stuff
 import tkinter as tk #Tkinter for GUI
 
 window = tk.Tk()
-window.geometry("300x170")
+window.geometry("300x200")
 
 #Diffraction grating formula nl = d * sin(theta)
 
@@ -23,43 +23,65 @@ def is_number(value):
     except ValueError:
         return False
 
+#Function to clear the buttons
+def clear_all_buttons():
+    button_list = [n, lam, d, theta]
+    for b in button_list:
+        b.delete(1.0, "end")
+
 #Math functions
 def find_theta():
     global values
     global theta
-    result = values['n'] * values['lam']
-    result /= values['d']
-    result = math.asin(result)
-    theta.delete(1.0, "end")
-    theta.insert(1.0, str(result))
+    try:
+        result = values['n'] * values['lam']
+        result /= values['d']
+        result = math.asin(result)
+        theta.delete(1.0, "end")
+        theta.insert(1.0, str(result))
+    except:
+        clear_all_buttons()
+        values = {}
     return result
 
 def find_n():
     global values
     global n
-    result = math.sin(values["theta"])
-    result *= values["d"]
-    result /= values["lam"]
-    n.delete(1.0, "end")
-    n.insert(1.0, str(result))
+    try:
+        result = math.sin(values["theta"])
+        result *= values["d"]
+        result /= values["lam"]
+        n.delete(1.0, "end")
+        n.insert(1.0, str(result))
+    except:
+        clear_all_buttons()
+        values = {}
     return result
 
 def find_d():
     global values
     global d
-    result = values["n"] * values["lam"]
-    result /= math.sin(values["theta"])
-    d.delete(1.0, "end")
-    d.insert(1.0, str(result))
+    try:
+        result = values["n"] * values["lam"]
+        result /= math.sin(values["theta"])
+        d.delete(1.0, "end")
+        d.insert(1.0, str(result))
+    except:
+        clear_all_buttons()
+        values = {}
     return result
 
 def find_lam():
     global values
     global lam
-    result = values["d"] * math.sin(values["theta"])
-    result /= values["n"]
-    lam.delete(1.0, "end")
-    lam.insert(1.0, str(result))
+    try:
+        result = values["d"] * math.sin(values["theta"])
+        result /= values["n"]
+        lam.delete(1.0, "end")
+        lam.insert(1.0, str(result))
+    except:
+        clear_all_buttons()
+        values = {}
     return result
 
 #Function to collect values we'll need later
@@ -92,30 +114,26 @@ def take_values():
                 func_dic[key]()
     else:
         values = {}
-     
 
-#Function to clear the buttons
-def clear_all_buttons():
-    button_list = [n, lam, d, theta]
-    for b in button_list:
-        b.delete(1.0, "end")
+#Create information UI elements
+conversion_label = tk.Label(text = "2.5 x 10^-6 is the same as 2.5e-06")
 
 #Create Label and text input for variables
 n_label = tk.Label(window, text = "n:")
 n_label.config(font = ("Calibri", 14))
-n = tk.Text(window, height = 1, width = 52)
+n = tk.Text(window, height = 1, width = 22)
 
 lam_label = tk.Label(window, text = "lambda:")
 lam_label.config(font = ("Calibri", 14))
-lam = tk.Text(window, height = 1, width = 52)
+lam = tk.Text(window, height = 1, width = 22)
 
 d_label = tk.Label(window, text = "d:")
 d_label.config(font = ("Calibri", 14))
-d = tk.Text(window, height = 1, width = 52)
+d = tk.Text(window, height = 1, width = 22)
 
 theta_label = tk.Label(window, text = "theta:")
 theta_label.config(font = ("Calibri", 14))
-theta = tk.Text(window, height = 1, width = 52)
+theta = tk.Text(window, height = 1, width = 22)
 
 
 calc_button = tk.Button(window, text = "Calculate!", command = take_values)
@@ -123,21 +141,28 @@ clear_button = tk.Button(window, text = "Clear values", command = clear_all_butt
 exit_button = tk.Button(window, text = "Exit", command = window.destroy)
 
 #Arrange all UI elements
-n_label.grid(column = 0, row = 0)
-n.grid(column = 1, row = 0)
+conversion_label.grid(row = 0, sticky = 'nsew')
 
-lam_label.grid(column = 0, row = 1)
-lam.grid(column = 1, row = 1)
+n_label.grid(column = 0, row = 1 , sticky = 'nsew')
+n.grid(column = 1, row = 1, sticky = 'ew')
 
-d_label.grid(column = 0, row = 2)
-d.grid(column = 1, row = 2)
+lam_label.grid(column = 0, row = 2, sticky = "nsew")
+lam.grid(column = 1, row = 2, sticky = "ew")
 
-theta_label.grid(column = 0, row = 3)
-theta.grid(column = 1, row = 3)
+d_label.grid(column = 0, row = 3, sticky = "nsew")
+d.grid(column = 1, row = 3, sticky = "ew")
 
-calc_button.grid(column = 0, row = 4)
-clear_button.grid(column = 1, row = 4, sticky = "w")
-exit_button.grid(row = 5)
+theta_label.grid(column = 0, row = 4, sticky = "nsew")
+theta.grid(column = 1, row = 4, sticky = "ew")
+
+calc_button.grid(column = 0, row = 5, sticky = "nsew")
+clear_button.grid(column = 1, row = 5, sticky = "nsew")
+exit_button.grid(row = 6, sticky = "nsew")
+
+#Handle resizing for specific rows and/or columns
+for i in range(6):
+    window.rowconfigure(i, weight = 1)
+    window.grid_columnconfigure(0, weight = 1)
 
 window.mainloop()
 
